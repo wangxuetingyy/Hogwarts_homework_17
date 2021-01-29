49fd4ad4214393891ae3183ea65dfe60cfdef45d
Ting <445057975@qq.com>
2021-01-30 00:06:53 +0800

    第一次提交作业(计算器加法和除法)

---
+++ README.md
@@ -0,0 +1,5 @@
+# 项目介绍
+霍格沃兹测试学院测开17期实战演示
+
+# 作业地址
+git@github.com:wangxuetingyy/Hogwarts_homework_17.git
--- testing/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# author： Ting
-# datetime： 2021/1/29 22:24 
-# ide： PyCharm
