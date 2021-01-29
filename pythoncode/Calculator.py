cad3dfd5bb1aefeb1accc1d8262293c3d06bcfdd
Ting <445057975@qq.com>
2021-01-30 00:01:12 +0800

    第一次提交作业(计算器加法和除法)

---
+++ pythoncode/Calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： Ting
+# datetime： 2021/1/29 22:31 
+# ide： PyCharm
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def minus(self, a, b):
+        return a - b
+
+    def multi(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
---
+++ testing/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： Ting
+# datetime： 2021/1/29 22:24 
+# ide： PyCharm
