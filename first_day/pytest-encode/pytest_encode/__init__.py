424a8cdb32ddac17cc5c6c668c76154a42ecf0d5
刘强 <qiang1.liu@bkjk.com>
2021-02-05 01:34:23 +0800

    hook函数

--- first_day/pytest-encode/pytest_encode/__init__.py
+++ first_day/pytest-encode/pytest_encode/__init__.py
@@ -4,11 +4,24 @@
 
 
 #  编码格式
-from typing import List
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    # 日志格式
+                    # 时间、代码所在文件名、代码行号、日志级别名字、日志信息
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    # 打印日志的时间
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    # 日志文件存放的目录（目录必须存在）及日志文件名
+                    filename='report.log',
+                    # 打开日志文件的方式
+                    filemode='w'
+                    )
+logger = logging.getLogger(__name__)
 
 
-def pytest_collection_modifyitems(session: "Session", config: "Config", items: List["Item"]) -> None:
-    print(items)
+def pytest_collection_modifyitems(items):
     for item in items:
-        item.name = item.name.encode('utf-8').decode('unicode-escape')
-        item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
+        item.name = item.name.encode('utf-8').decode('unicode_escape')
+        item._nodeid = item._nodeid.encode('utf-8').decode('unicode_escape')
+
