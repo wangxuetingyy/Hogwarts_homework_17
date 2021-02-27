27a3c3c65a4a66cf2306eda8bdf4b2b4738efa96
Ting <445057975@qq.com>
2021-02-28 01:28:44 +0800

    浏览器复用、使用cookie登录

--- two_day/test_selenium/selenium_demo.py
+++ two_day/test_selenium/selenium_demo.py
@@ -11,26 +11,35 @@ from selenium import webdriver
 
 
 class TestSelenium:
-    def setup_method(self, method):
-        # 浏览器复用
+    def setup_method(self):
+        """
+        一、打开浏览器 chrome --remote-debugging-port=9222
+        # 二、python复用浏览器
+        :return:
+        """
         chrome_arg = webdriver.ChromeOptions()
         chrome_arg.debugger_address = '127.0.0.1:9222'
         self.driver = webdriver.Chrome(options=chrome_arg)
         self.driver.maximize_window()
 
-    def teardown_method(self, method):
+    def teardown_method(self):
         self.driver.quit()
 
-    def test_cookie(self):
-        # 获取cookie
-        self.driver.get("https://work.weixin.qq.com/wework_admin/frame")
-        time.sleep(10)
+    def test_get_cookie(self):
+        """
+        登录后,获取cookie
+        :return:
+        """
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
         cookies_list = self.driver.get_cookies()
         with open('data.txt', 'w', encoding='utf-8')as f:
-            json.dump(cookies_list, f)
+            json.dump(cookies_list, f)  # 传入流
 
-    def test_cookie_login(self):
-        # 使用cookie登录
+    def test_login_cookie(self):
+        """
+        使用cookie登录
+        :return:
+        """
         self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
         with open('data.txt', "r", encoding="utf-8") as f:
             cookies_list = json.load(f)
@@ -38,4 +47,4 @@ class TestSelenium:
             self.driver.add_cookie(cookie)
         self.driver.refresh()
         time.sleep(5)
-        self.driver.find_element_by_xpath("//*[@id='menu_contacts']").click()
+
